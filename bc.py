5be6e5d92316507394e9119ae23b28c13ff82f2e
mcwasa <101937045+mcwasa@users.noreply.github.com>
2024-07-10 15:58:15 +0200

    Update bc.py

--- bc.py
+++ bc.py
@@ -584,7 +584,8 @@ def p4():
                 else:pwv.append(nmf);pwv.append(frs+'123');pwv.append(frs+'1234');pwv.append(frs+'123456');pwv.append(frs+'12345');pwv.append(nmf+'1234')
             else:
                 if len(frs)<3:pwv.append(nmf)
-                else:pwv.append(nmf)')
+                else:pwv.append(nmf);pwv.append(frs+'123');pwv.append(frs+'1234');pwv.append(frs+'123456');pwv.append(frs+'12345')
+
             if 'ya' in pwpluss:
                 for xpwd in pwnya:pwv.append(xpwd)
             else:pass
