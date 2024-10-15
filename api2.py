813ec3f8a7df6018244b65b74aef87cfc8b9d324
mcwasa <101937045+mcwasa@users.noreply.github.com>
2024-10-16 00:51:20 +0100

    Update api2.py

--- api2.py
+++ api2.py
@@ -168,7 +168,6 @@ def menu():
                                         plist.append('first last')
                                         plist.append('firstlast')
                                         plist.append('first123')
-					plist.append('first1234')
                                         plist.append('first12345')
                                         plist.append('first123456')
                                 else:
